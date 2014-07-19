217ec1d809c7356938103f68dc39f0d3e731025b
AndrewRademacher <andrewrademacher@gmail.com>
2014-07-19 15:31:10 -0500

    Added new manager route.

--- server/routes/manager.js
+++ server/routes/manager.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var schema = require('json-schema');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-    res.json({
-        message: 'Hello, World!'
-    });
+router.post('/', function(req, res) {
+    var report = schema.validate(req.body, req.app.get('schema').Manager);
+    if (!report.valid) return app.json(400, report.errors);
+
+    var manager = req.app.get('db').manager;
+    manager.findOne({ rdio_oauth: req.body.rdio_oauth }, { _id: 1, rdio_oauth: 1 })
+    .then(function(doc) {
+        if (doc) res.json(doc);
+        else return manager.insert(req.body);
+    })
+    .then(function(doc) {
+        res.json({
+            _id: doc._id,
+            rdio_oauth: rdio_oauth
+        });
+    }, function(err) {
+        res.json(500, err);
+    })
 });
 
 module.exports = router;
---
+++ server/test/manager.js
@@ -0,0 +1,28 @@
+var should = require('should');
+var request = require('request');
+
+var hostname = 'localhost',
+    port = 3000,
+    domain = 'http://' + hostname + ':' + port;
+
+describe('R2DJ', function() {
+    describe('Manager', function() {
+
+
+        it('should create a user with RDIO oauth.', function(done) {
+            request({
+                url: domain + '/manager',
+                method: 'POST',
+                json: {
+                    rdio_oauth: 'foauth234'
+                }
+            }, function(err, res, body) {
+                should.not.exist(err);
+                should(res.statusCode).equal(200);
+                should(body).have.property('_id');
+                should(body).have.property('rdio_oauth');
+                done();
+            });
+        });
+    });
+});
