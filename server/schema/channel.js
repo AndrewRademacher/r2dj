6107118f9c0fd6411164e4205508d447aa5708d9
AndrewRademacher <andrewrademacher@gmail.com>
2014-07-19 17:22:15 -0500

    Updated channel schema.

--- server/schema/channel.js
+++ server/schema/channel.js
@@ -2,9 +2,41 @@ module.exports = {
     title: 'Channel',
     type: 'object',
     properties: {
-        _id: { type: 'string' },
-        ownerId: { type: 'string', required: true },
-        name: { type: 'string', required: true }
+        _id: {
+            type: 'string'
+        },
+        ownerId: {
+            type: 'string',
+            required: true
+        },
+        name: {
+            type: 'string',
+            required: true
+        },
+        history: {
+            type: 'array',
+            value: {
+                type: 'object'
+                properties: {
+                    _id: { type: 'string', required: true },
+                    artist: { type: 'string', required: true },
+                    title: { type: 'string', required: true }
+                },
+                additionalProperties: false
+            }
+        },
+        queue: {
+            type: 'array',
+            value: {
+                type: 'object',
+                properties: {
+                    _id: { type: 'string', required: true },
+                    artist: { type: 'string', required: true },
+                    title: { type: 'string', required: true }
+                }
+                additionalProperties: false
+            }
+        }
     },
     additionalProperties: false
 };
