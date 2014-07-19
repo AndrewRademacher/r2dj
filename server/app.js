840cb1fd08b35a7ef019aec06e52bc003583cf4c
AndrewRademacher <andrewrademacher@gmail.com>
2014-07-19 13:37:56 -0500

    Removed static resources, switched all routes to json.

--- server/app.js
+++ server/app.js
@@ -11,16 +11,12 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.json({
+        name: 'R2DJ',
+        serverTime: new Date(),
+        version: '0.1.0'
+    });
 });
 
 module.exports = router;
