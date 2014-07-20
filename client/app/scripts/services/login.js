8e6d7b50d898a7655dd40809e575d6dda68c8e0c
AndrewRademacher <andrewrademacher@gmail.com>
2014-07-20 03:56:57 -0500

    Fixed app auth.

--- client/app/scripts/services/login.js
+++ client/app/scripts/services/login.js
@@ -11,13 +11,13 @@ angular.module('clientApp')
     .service('Login', function Login(localStorageService, Manager) {
         function dropAuthentication() {
             localStorageService.remove('auth.userId');
-            localStorageService.remove('auth.rdioKey');
+            localStorageService.remove('auth.rdioUser');
             localStorageService.remove('auth.rdioOauth');
         }
 
         function loginHome(currentUser) {
             (new Manager({
-                rdioKey: R.currentUser.get('key')
+                rdioUser: R.currentUser.get('key')
             })).$save(function(res) {
                 localStorageService.set('auth.userId', res._id);
             });
@@ -30,7 +30,7 @@ angular.module('clientApp')
                 }
 
                 localStorageService.set('auth.rdioOauth', R.accessToken());
-                localStorageService.set('auth.rdioKey', R.currentUser.get('key'));
+                localStorageService.set('auth.rdioUser', R.currentUser.get('key'));
                 loginHome(R.currentUser);
             });
         });
@@ -38,9 +38,9 @@ angular.module('clientApp')
         var $this = {
             isLoggedIn: function() {
                 var userId = localStorageService.get('auth.userId'),
-                    rdioKey = localStorageService.get('auth.rdioKey'),
+                    rdioUser = localStorageService.get('auth.rdioUser'),
                     rdioOauth = localStorageService.get('auth.rdioOauth');
-                return (userId !== null) && (rdioKey !== null);
+                return (userId !== null) && (rdioUser !== null);
             },
             login: function(callback) {
                 R.ready(function() {
